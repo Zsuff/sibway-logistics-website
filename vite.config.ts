c245e2d1a74baa57134c0dab115ae269e578c4c4
Zsuff <galagyz@gmail.com>
2026-08-25 22:52:46 +0300

    Налаштувати GitHub Pages для деплою сайту

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,16 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react()],
+  base: '/sibway-logistics-website/',
   server: {
     port: 5173,
-    host: true,
+    host: true
   },
-});
+  build: {
+    outDir: 'dist',
+    sourcemap: true
+  }
+})
